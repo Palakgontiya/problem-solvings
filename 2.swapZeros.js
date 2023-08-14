7ab55ab06695def7015e7a187058ec5772a37e91
Palakgontiya <palak.gontiya@gmail.com>
2023-08-14 19:42:59 +0530

    added 2 more files

--- 2.swapZeros.js
+++ 2.swapZeros.js
@@ -1,16 +1,27 @@
-let a = [1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 1, 1, 0, 0];
+// let a = [1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 1, 1, 0, 0];
 
-function Swap(arr) {
-  let index = 0;
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] === 1) {
-      let temp = arr[i];
-      arr[i] = arr[index];
-      arr[index] = temp;
-      index++;
-    }
+// function Swap(arr) {
+//   let index = 0;
+//   for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] === 1) {
+//       let temp = arr[i];
+//       arr[i] = arr[index];
+//       arr[index] = temp;
+//       index++;
+//     }
+//   }
+//   console.log(arr);
+// }
+
+// Swap(a);
+
+var x = 10;
+function test() {
+  if (x > 20) {
+    let x = 50;
   }
-  console.log(arr);
-}
 
-Swap(a);
+  console.log(x);
+  return x;
+}
+test(); //undefined
