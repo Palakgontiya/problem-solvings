3d1b8fe154ac60a7b619c13bf61b191542ab72e7
Palakgontiya <palak.gontiya@gmail.com>
2023-08-16 18:52:24 +0530

    added new file 4.largestInteger.js

---
+++ 4.largestInteger.js
@@ -0,0 +1,26 @@
+/**we have an array of elements we need to find the k'th largest element run a
+loop through the elements and run k times find the first largest element
+eliminate the largest elements untill the loop is runing for k'th time**/
+function returnKthElement(nums, k) {
+  let largestElem = -10000;
+  for (let i = 0; i < k; i++) {
+    let largestIndex = 0;
+    largestElem = -10000;
+
+    for (let j = 0; j < nums.length; j++) {
+      // Start from index 1
+      if (nums[j] >= largestElem) {
+        largestIndex = j;
+        largestElem = nums[j];
+      }
+    }
+
+    nums.splice(largestIndex, 1);
+  }
+  return largestElem;
+}
+
+const num = [3, 2, 3, 1, 2, 4, 5, 5, 6];
+const k = 5;
+
+console.log(returnKthElement(num, k)); // Output: 4
